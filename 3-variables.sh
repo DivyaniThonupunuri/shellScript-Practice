c357236a206fe26dde38f00e4be7735b662603c9
Divyani <divyani.t74@gmail.com>
2025-05-15 17:18:10 +0530

    shell-scripting

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name1=Trump
+name2= Musk
+
+echo "$name1:: Hey $name2, How are you?"
+echo "$name2:: Hello $name1, I am fine. How are you doing"
+echo "$name1: I am fine too. What's up?"
+echo "$name2:: Nohting, just going to Mars now, will you come?"
+echo "$name1:: Sorry, you carry on! I will come once you come back"
