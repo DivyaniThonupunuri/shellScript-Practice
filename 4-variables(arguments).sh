dc67f814df9ec8acc262f6da1453f1ec9bf52d56
Divyani <divyani.t74@gmail.com>
2025-05-15 19:01:10 +0530

    shell-scripting

---
+++ 4-variables(arguments).sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name1=$value1
+name2=$value2
+
+echo "$name1:: Hey $name2, How are you?"
+echo "$name2:: Hello $name1, I am fine. How are you doing"
+echo "$name1: I am fine too. What's up?"
+echo "$name2:: Nohting, just going to Mars now, will you come?"
+echo "$name1:: Sorry, you carry on! I will come once you come back"
