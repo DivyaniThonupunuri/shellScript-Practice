79005c31e1c1fc3eedfd76c4c8804b184406128f
Divyani <divyani.t74@gmail.com>
2025-05-19 18:32:33 +0530

    conditional loop

--- 8-condition.sh
+++ 8-condition.sh
@@ -3,7 +3,7 @@
 #if condition to check number is greater than  10 or not , pass number dynamically 
 
 num =$1 
- if [ $num -gt 10 ]
+ if [ $num -lt 10 ]
  then 
 
  echo "num $num is greater than 10"
