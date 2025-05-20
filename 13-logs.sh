47ee4e83d31457cc8b28c4eba8db56b217752127
Divyani <divyani.t74@gmail.com>
2025-05-20 14:13:03 +0530

    colours

--- 13-logs.sh
+++ 13-logs.sh
@@ -8,7 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shell_scriptlogs"
-SCRIPT_NAME=$(echo $0 | | cut -d '.' -f1 )
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER 
 echo ("script started at : $(date)") &>>LOG_FILE
