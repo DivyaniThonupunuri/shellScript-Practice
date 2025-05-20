76046a1d050425db760e49c38ced74f11a27f106
Divyani <divyani.t74@gmail.com>
2025-05-20 15:07:15 +0530

    loop

--- 17.sh
+++ 17.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 PACKAGES=("mysql" "python" "nginx" "httpd")
 for package in $@
 do
-    dnf list installed $package &>>$LOG_FILE
+    dnf list installed $package 
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed... going to install it" | tee -a $LOG_FILE
+        echo "$package is not installed... going to install it" | tee 
         dnf install $package -y &>>$LOG_FILE
-        VALIDATE $? "$package"
-    else
-        echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
+        
+    else        echo -e "Nothing to do $package... $Y already installed $N" | tee 
     fi
 done
