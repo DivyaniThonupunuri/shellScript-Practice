2f4272c5f4958042e8a6a0473944dc18664f05f6
Divyani <divyani.t74@gmail.com>
2025-05-27 17:30:41 +0530

    delete logs

--- 18-delete_old_logs.sh
+++ 18-delete_old_logs.sh
@@ -1,9 +1,29 @@
 #!/bin/bash 
 
-Source_directory="/var/log/applogs"
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+Source_directory="/home/ec2-user/applogs"
+
+mkdir $LOGS_FOLDER
+echo "Script started executing at $(date)" | tee -a $LOG_FILE
+
 files_to_delete=$(find $Source_directory -name "*.log" -mtime +14)
 
 while IFS= read -r filepath
 do 
     rm -rf $filepath
 done <<< $files_to_delete
+
+if [ $userid -ne 0 ]
+then 
+    echo -e " $R ERROR: Run the script with root access $N "| tee -a $LOG_FILE
+    exit 1 ; 
+else 
+    echo -e "$G you are running with root " | tee -a $LOG_FILE
+fi
