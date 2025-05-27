6bd1bafe859d00499eeaf259805cae45bc37864b
Divyani <divyani.t74@gmail.com>
2025-05-27 23:31:44 +0530

    delete logs

--- 19-disk_usage.sh
+++ 19-disk_usage.sh
@@ -1,14 +1,14 @@
 #/!bin/bash 
  #-v is used to remove filesystem line 
 Disk_usage=$(df -hT | grep -v Filesystem)
-Disk_thresold=5
+Disk_threhold=5
 msg=""
 
 while IFS= read line 
 do
     USAGE=$( echo $line | awk '{print $6F}'| cut -d "%" -f1 )
     PARTITION=$(echo $line | awk '{print $7F}')
-    if [ $USAGE -ge $Disk_thresold ]
+    if [ $USAGE -ge $Disk_threhold ]
     then 
         echo="High disk usage on $PARTITION:$USAGE % <br>"
     fi
