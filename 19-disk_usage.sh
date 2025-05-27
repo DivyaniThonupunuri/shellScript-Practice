1f60d75702ce75440a961e438f356174fbdc5d53
Divyani <divyani.t74@gmail.com>
2025-05-27 23:30:36 +0530

    delete logs

--- 19-disk_usage.sh
+++ 19-disk_usage.sh
@@ -8,7 +8,7 @@ while IFS= read line
 do
     USAGE=$( echo $line | awk '{print $6F}'| cut -d "%" -f1 )
     PARTITION=$(echo $line | awk '{print $7F}')
-    if [ $USAGE -ge $Disk_thresold ];
+    if [ $USAGE -ge $Disk_thresold ]
     then 
         echo="High disk usage on $PARTITION:$USAGE % <br>"
     fi
